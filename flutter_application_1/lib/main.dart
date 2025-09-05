dcd012836c0308fb158422023120d036ebba5c54
Kedirbiruh <kedirbiruh1114@gmail.com>
2025-09-05 13:27:07 +0200

    start counter app

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -6,22 +6,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(232, 4, 132, 245), // für appBar
+          seedColor: const Color.fromARGB(255, 19, 18, 18),
         ),
-        // primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: const Color.fromARGB(
-          255,
-          84,
-          83,
-          83,
-        ), //     für body
+        scaffoldBackgroundColor: const Color.fromARGB(255, 177, 207, 228),
       ),
       home: const MyHomePage(title: 'Counter App'),
     );
@@ -38,41 +33,84 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  void plusCounter() {
+
+  void _decrementCounter() {
     setState(() {
-      _counter++;
+      // _counter--;
+      _counter -= 2;
     });
   }
 
-  void minusCounter() {
+  void _resetCounter() {
     setState(() {
-      _counter--;
+      _counter = 0;
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      // _counter++;
+      _counter += 2;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title), // wird title von my homepage rein gegeben
-      ),
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>[
             const Text(
-              'click the on button',
-              style: TextStyle(color: Colors.white),
+              'Counter App',
+              // style: TextStyle(color: Colors.white),
             ),
             Text(
-              '$_counter', // counter 0 bis counter ++
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineMedium, // aus material Theme kriegt er style textTheme --headlineMedium
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+              // aus material Theme kriegt er style textTheme --headlineMedium
             ),
-            ElevatedButton(onPressed: plusCounter, child: const Text('Next')),
+
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // ElevatedButton(onPressed: _decrementCounter, child: Text("-")),
+                ElevatedButton(
+                  onPressed: _decrementCounter,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 223, 123, 116),
+                    padding: const EdgeInsets.all(5),
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  child: const Icon(Icons.remove),
+                ),
+
+                SizedBox(width: 10),
+
+                ElevatedButton(
+                  onPressed: _resetCounter,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 245, 206, 77),
+                    padding: const EdgeInsets.all(5),
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  child: const Icon(Icons.refresh),
+                ),
+
+                SizedBox(width: 10),
+
+                ElevatedButton(
+                  onPressed: _incrementCounter,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 116, 223, 143),
+                    padding: const EdgeInsets.all(5),
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
           ],
         ),
       ),
